e6f0aad8515610ae3ca119351dd46659de9024d8
Essabuloushi <124120519+Essabuloushi@users.noreply.github.com>
2023-07-07 16:14:35 -0700

    Add files via upload

--- hw08/pointing.h
+++ hw08/pointing.h
@@ -20,17 +20,62 @@
     const int TIC_TAC_TOE_SIZE = 3;
     // function definitions:
 
+    /**************************************************************************
+    *   Increment Value
+    * _________________________________________________________________________
+    *   This function passes in a variable and increments the value of it.
+    ***************************************************************************/
     int increment_value(int x);
+    /**************************************************************************
+    *  Increment Pointer
+    * _________________________________________________________________________
+    * This function passes in a pointer and increments the value of the 
+    * pointer variable
+    ***************************************************************************/
     void increment_pointer(int* p);
+    /**************************************************************************
+    *  Increment Reference
+    * _________________________________________________________________________
+    *  This function passes a pointer by reference and directly changes 
+    *  the value changed in the variable
+    ***************************************************************************/
     void increment_reference(int& r);
+   /**************************************************************************
+    * Print 2d Array Subscripts 
+    * _________________________________________________________________________
+    * This function passes in a 2d array and prints the values stored in the
+    * array by dereferencing
+    ***************************************************************************/
     void print_2darray_subscript(double twoDD[][ARRAY_SIZE], int row, int col);
+    /**************************************************************************
+    *  Print 2darray pointer
+    * _________________________________________________________________________
+    *   This function prints a 2d array by pointer to the blocks of memory
+    *   and printing whatever is stored in the values
+    ***************************************************************************/
     void print_2darray_pointer(double* twoDD, int row, int col);
-
+    /**************************************************************************
+    *  Print 2d array Dynamic Subscript
+    * _________________________________________________________________________
+    * This function prints the 2d array using pointer arithmetic and
+    * returns the values stored in the memory addresses 
+    ***************************************************************************/
     void print_2darray_dynamic_subscript(int** twoDD, int row, int col);
+    /**************************************************************************
+    * Print 2d array dynamic pointer 
+    * _________________________________________________________________________
+    * This function prints the 2d array pointers and their memory addresses
+    ***************************************************************************/
     void print_2darray_dynamic_pointer(int** twoDD, int row, int col);
+    /**************************************************************************
+    * Dynamic allocation array doubles   
+    * _________________________________________________________________________
+    * This function dynamically allocates memory for an array and its size
+    * by using array_size which is passed in from main
+    ***************************************************************************/
     double* dynamic_allocation_array_doubles(int array_size);
-    int* pi_array = new int[DYNAMIC_SIZE];
-    double* pd_array = new double[ARRAY_SIZE];
+    int* pi_array = new int[DYNAMIC_SIZE]; //Dynamically allocates int array
+    double* pd_array = new double[ARRAY_SIZE];  //Dynamically allocates double array
     }
 
 #endif
